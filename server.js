dd1e4e8b53bd54123bb096cb4245b7982222ad85
George Sanchez <petabyte.gs@gmail.com>
2016-02-14 01:41:44 -0600

    Updated dependencies Added moment

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // Load the necessary servers.
 var mqtt = require("mqtt");
+var moment = require("moment");
 var elasticsearch = require('elasticsearch');
  
 //Create our elasticsearch Client
 var elasticSearchClient = new elasticsearch.Client({
-  host: 'localhost:9200',
+  host: '172.31.19.77:9200',
   log: 'trace'
 });
 
 // Create our MQ Client.
 var clientIdVar = "myclientid_" + parseInt(Math.random() * 100, 10);
-var mqClient = mqtt.connect({host:'localhost', port:1883, clientId: clientIdVar});
+var mqClient = mqtt.connect("mqtt://172.31.19.77:1883",{protocolId: 'MQIsdp',protocolVersion: 3});
 mqClient.subscribe("events_messages");
 //
 mqClient.on('connect',  function () {
@@ -23,14 +24,17 @@ mqClient.on('message',  function (topic, queueMessage) {
  	 console.log("This is the queueMessage " + queueMessage);
      queueMessage = JSON.parse(queueMessage);
      //Do something with the push message you received
+     var now = moment().format();
      elasticSearchClient.create({
-     	"index": "events",
+     	"index": "iot_events",
      	"type":"events_messages",
      	 "body":{
      	 	"event_id": clientIdVar,
      	 	"message_type": queueMessage.type,
      	 	"message": queueMessage.message,
-     	 	"product": queueMessage.product
+     	 	"product": queueMessage.product,
+                "value": queueMessage.value,
+                "postDate":now
      	 }
      	}, function (error, response){
      	 	console.log("This is the response " + error + " " +response);
